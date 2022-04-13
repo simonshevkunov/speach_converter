5ced5eaf897355f9bbd6121c054f1edd6334101c
BillyYank <>
2022-04-13 22:57:42 +0500

    comment

--- bot.py
+++ bot.py
@@ -5,13 +5,10 @@ API_TOKEN = '5180015716:AAHEXPed8DWwCRabaLpbdJRqXCKepPYrxhI'
 bot = telebot.TeleBot(API_TOKEN)
 
 
-
+# TODO add voice
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-    bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
-""")
+    bot.reply_to(message, """Hi""")
 
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
