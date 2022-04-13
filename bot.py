054072aeb10f8d5c32e981801fb1fc57baabde23
simonshevkunov <103308980+simonshevkunov@users.noreply.github.com>
2022-04-13 20:56:03 +0300

    code update

--- bot.py
+++ bot.py
@@ -1,2 +1,23 @@
-if __name__ == "__main__":
-    print("hello world")
+import telebot
+
+API_TOKEN = '5180015716:AAHEXPed8DWwCRabaLpbdJRqXCKepPYrxhI'
+
+bot = telebot.TeleBot(API_TOKEN)
+
+
+
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
+
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+bot.infinity_polling()
