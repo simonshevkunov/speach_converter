01ec475e0286ad36335b64d9696b92842634f500
simonshevkunov <103308980+simonshevkunov@users.noreply.github.com>
2022-04-23 09:41:59 +0300

    bot_problem

--- bot.py
+++ bot.py
@@ -1,4 +1,11 @@
 import telebot
+import requests
+import json
+
+IAM_TOKEN = 't1.9euelZqPnYmXx8mSzc2Sy5yWi5bLyO3rnpWamp6Tj5zMypHHzJGSxpCby8rl9PcXU39s-e9zKC2C3fT3VwF9bPnvcygtgg.3vW6t4osf5ob0K0kvjXTPCiNzL_SkyZS95vVwSxnk5TnFJv5OTvPptdWP_1Qdcqj7crESVkUIGsJ-y-sMUXJDA'
+FOLDER_ID = 'b1gtbkakskb978s7geb5'
+
+
 
 API_TOKEN = '5366935243:AAF-kbdlkr-8bl08eazYFlWTh-SDjCwdXe8'
 
@@ -16,5 +23,17 @@ def voice_processing(message):
     downloaded_file = bot.download_file(file_info.file_path)
     with open('new_file.ogg', 'wb') as new_file:
         new_file.write(downloaded_file)
+
+headers = {
+    "Authorization": f"Bearer {IAM_TOKEN}"
+}
+with open('new_file.ogg', 'rb') as f:
+    resp = requests.post(f"https://stt.api.cloud.yandex.net/speech/v1/stt:recognize?folderId={FOLDER_ID}&lang=ru-RU",
+                 data=f.read(),
+                        headers=headers)
+    res = json.loads(resp.content)
+
+def convert_speech(message):
+    bot.reply_to(message, res)
 
 bot.infinity_polling()
